05f3f2bc766547a6c4d44164930f81e8390ef8aa
Vladimir Jimenez <allejo@me.com>
2020-07-11 21:31:27 -0700

    Take stroke widths into account in node sizes

--- src/graphics/SVG/SVGStylableUtilities.php
+++ src/graphics/SVG/SVGStylableUtilities.php
@@ -35,6 +35,31 @@ abstract class SVGStylableUtilities
         {
             $node->setAttribute('stroke', $color);
             $node->setAttribute('stroke-width', (string)$width);
+
+            //
+            // Manipulate our SVGNode to shrink its size to account for the extra width added by the object stroke
+            //
+
+            // Get current values
+            $nodeX = (int)$node->getAttribute('x');
+            $nodeY = (int)$node->getAttribute('y');
+            $nodeHeight = (int)$node->getAttribute('height');
+            $nodeWidth = (int)$node->getAttribute('width');
+
+            // Move the X + Y positions by half of the stroke width. Strokes in SVGs are applied at the center of the
+            // path, meaning half the stroke will be on the inside of the object and the other half on the outside of
+            // the path.
+            $newX = $nodeX + ($width / 2);
+            $newY = $nodeY + ($width / 2);
+
+            // Calculate the size of the node by taking into account the stroke on both sides
+            $newHeight = $nodeHeight - $width;
+            $newWidth = $nodeWidth - $width;
+
+            $node->setAttribute('x', (string)$newX);
+            $node->setAttribute('y', (string)$newY);
+            $node->setAttribute('height', (string)$newHeight);
+            $node->setAttribute('width', (string)$newWidth);
         }
     }
 }
