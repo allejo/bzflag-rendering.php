bf238e26d6f03b6ce3f162132d88f9313f88fda3
RighthandSon <59427094+RighthandSon@users.noreply.github.com>
2021-08-27 15:10:28 -0500

    add mesh renderer?

---
+++ src/graphics/SVG/Radar/MeshRenderer.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+
+/*
+ * (c) Vladimir "allejo" Jimenez <me@allejo.io>
+ *
+ * For the full copyright and license information, please view the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+namespace allejo\bzflag\graphics\SVG\Radar;
+
+use allejo\bzflag\graphics\Common\WorldBoundary;
+use allejo\bzflag\graphics\SVG\ISVGStylable;
+use allejo\bzflag\graphics\SVG\Radar\Styles\MeshStyle;
+use allejo\bzflag\graphics\SVG\SVGStylableUtilities;
+use allejo\bzflag\world\Object\MeshObstacle;
+use SVG\Nodes\Shapes\SVGPolygon;
+use SVG\Nodes\SVGNode;
+
+/**
+ * @internal
+ *
+ * @extends ObstacleRenderer<\allejo\bzflag\world\Object\MeshObstacle>
+ * @implements ISVGStylable<\allejo\bzflag\world\Object\MeshObstacle>
+ */
+class MeshRenderer extends ObstacleRenderer implements ISVGStylable
+{
+    /** @var MeshStyle */
+    public static $STYLE;
+
+    /** @var MeshObstacle */
+    protected $obstacle;
+
+    /**
+     * @param MeshObstacle $mesh
+     */
+    public function __construct($mesh, WorldBoundary $worldBoundary)
+    {
+        if (self::$STYLE === null)
+        {
+            self::$STYLE = new MeshStyle();
+        }
+
+        parent::__construct($mesh, $worldBoundary);
+    }
+
+    public function exportSVG(): SVGNode
+    {
+        $svg = $this->objectToSvgNode(SVGPolygon::class);
+
+        self::stylizeSVGNode($svg, $this->obstacle);
+
+        if ($this->bzwAttributesEnabled)
+        {
+            self::attachBzwAttributes($svg, $this->obstacle);
+        }
+
+        return $svg;
+    }
+
+    /**
+     * @param null|MeshObstacle $obstacle
+     */
+    public static function attachBzwAttributes(SVGNode $node, $obstacle): void
+    {
+        //$node->setAttribute('bzw:checktypes', implode(' ', $obstacle->getCheckTypes()));
+        //$node->setAttribute('bzw:checkpoints', implode(' ', $obstacle->getCheckPoints()));
+        //$node->setAttribute('bzw:vertexcount', (string)$obstacle->getVertexCount());
+        $node->setAttribute('bzw:vertices', implode(' ', $obstacle->getVertices()));
+        //$node->setAttribute('bzw:normalcount', (string)$obstacle->getNormalCount());
+        $node->setAttribute('bzw:normals', implode(' ', $obstacle->getNormals()));
+        //$node->setAttribute('bzw:texcoords', implode(' ', $obstacle->getTexCoords()));
+        $node->setAttribute('bzw:facesize', (string)$obstacle->getFaceSize());
+        $node->setAttribute('bzw:faces', implode(' ', $obstacle->getFaces()));
+    }
+
+    /**
+     * @param null|MeshObstacle $obstacle
+     */
+    public static function stylizeSVGNode(SVGNode $node, $obstacle): void
+    {
+        SVGStylableUtilities::applyFill($node, self::$STYLE->getFillColor());
+        SVGStylableUtilities::applyStroke($node, self::$STYLE->getBorderColor(), self::$STYLE->getBorderWidth());
+    }
+}
